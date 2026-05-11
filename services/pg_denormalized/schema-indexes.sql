d255270c2296b4abc9cda042b2ceb61caf0624fa
khodge1607 <kgraceh.04@gmail.com>
2026-05-11 10:44:47 -0700

    cleaned-ish schema-indexes files

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -5,12 +5,13 @@ SET max_parallel_workers = 80;
 CREATE INDEX ON tweets_jsonb
 USING gin((data->'entities'->'hashtags') jsonb_path_ops);
 
-CREATE INDEX ON tweets_jsonb
-USING gin((data->'extended_tweet'->'entities'->'hashtags') jsonb_path_ops);
+CREATE INDEX idx_jsonb_id ON tweets_jsonb((data->>'id'));
 
 CREATE INDEX ON tweets_jsonb((data->>'lang'));
 
 CREATE INDEX ON tweets_jsonb
 USING gin(to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text')));
 
-CREATE INDEX idx_jsonb_id ON tweets_jsonb((data->>'id'));
+CREATE INDEX ON tweets_jsonb
+USING gin((data->'extended_tweet'->'entities'->'hashtags') jsonb_path_ops);
+
--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -5,3 +5,6 @@ SET max_parallel_workers = 80;
 CREATE INDEX ON tweets(id_tweets, lang); --for query 3
 CREATE INDEX ON tweets USING gin(to_tsvector('english', text)); --for query4
 CREATE INDEX ON tweet_tags(tag, id_tweets); --for query 1, 2
+
+CREATE INDEX ON tweets(id_tweets);
+CREATE INDEX ON tweet_tags(id_tweets);
